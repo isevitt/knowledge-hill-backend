452096f2854f2681e77d09946e1291d652b7d431
itaise <itaise@wix.com>
2024-08-11 17:09:47 +0300

    add random item

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from typing import Optional
-
+from mock_db import mock_items
 from fastapi import FastAPI
-
+import random
 app = FastAPI()
 
 
@@ -10,5 +10,17 @@ async def root():
     return {"message": "Hello World"}
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
+def get_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("items/next_item")
+def get_next_item():
+    # currently gets a random entruy from the mock_items list
+    random_item: dict = get_random_item()
+    return random_item
+
+
+def get_random_item() -> dict:
+    random_item = mock_items[random.randint(0, len(mock_items) - 1)]
+    return random_item
---
+++ mock_db.py
@@ -0,0 +1,16 @@
+mock_items = [
+    {"item_id": "aaac1b1e-1b1b-1b1b-1b1b-1b1b1b1b1b1b",
+     "name": "How to grow tomatoes in a small balcony",
+     "description": "The main thing tomatoes need is sun. And I also recommend a fertilizer with lots of XYZ",
+     "date_created": "2021-01-01 10:10:10.111"},
+
+    {"item_id": "bbbc1b1e-1b1b-1b1b-1b1b-1b1b1b1b1b1b",
+     "name": "How to grow basil indoors",
+     "description": "Basil is a very easy plant to grow indoors. Just make sure it gets plenty of water",
+     "date_created": "2021-01-01 10:10:10.111"},
+
+    {"item_id": "ccac1b1e-1b1b-1b1b-1b1b-1b1b1b1b1b1b",
+     "name": "How to grow a lemon tree from seed",
+     "description": "Growing a lemon tree from seed is a fun project. Just make sure you have a lot of patience",
+     "date_created": "2021-01-01 10:10:10.111"}
+]
